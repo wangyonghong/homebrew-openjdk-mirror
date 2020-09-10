0b33d4b7a9c0bab2b8a84260bc059b57013f659d
wangyonghong <yonghongwang@163.com>
2020-09-10 18:35:30 +0800

    feat: add update script

---
+++ README.md
@@ -0,0 +1,18 @@
+# Homebrew Tap [wangyonghong/openjdk-mirror](https://github.com/wangyonghong/homebrew-openjdk-mirror)
+
+## 如何安装
+
+`brew install wangyonghong/openjdk/<formula>`
+
+Or `brew tap wangyonghong/openjdk` and then `brew install <formula>`.
+
+例子：
+
+```
+brew search openjdk
+brew cask install adoptopenjdk8
+```
+
+## Todo
+
+- 每天自动更新发 pull request
--- update_casks.py
+++ update_casks.py
@@ -1,19 +1,47 @@
 from os import replace
-import re
-import yaml
+import requests
 
-with open('versions.yml', 'r', encoding='utf-8') as f:
-  versions = yaml.load(f.read(), Loader=yaml.Loader)
-  for version in versions:
-    print(version)
-    with open('Templates/adoptopenjdk.tmpl.rb') as f:
-      cask = f.read().replace('{cask_name}', 'tuna-' + version['cask_name'])\
-        .replace('{version}', version['version'])\
-        .replace('{sha256}', version['sha256'])\
-        .replace('{url}', version['tuna-url'])\
-        .replace('{name}', version['name'])\
-        .replace('{filename}', version['filename'])\
-        .replace('{identifier}', version['identifier'])
+with open('Templates/adoptopenjdk.tmpl.rb') as f:
+  cask = f.read()
 
-    with open('Casks/tuna-' + version['cask_name'] + '.rb', "w") as f:
-      f.write(cask)
+for ver in range(8, 15):
+  for jvm_impl in ('hotspot', 'openj9'):
+    r = requests.get(f"https://api.adoptopenjdk.net/v3/assets/latest/{ver}/{jvm_impl}", timeout=(5, 10))
+    r.raise_for_status()
+    rel_list = r.json()
+    for rel in rel_list:
+      binary = rel['binary']
+      if binary['image_type'] not in ('jre', 'jdk'): continue
+      if binary['architecture'] != 'x64': continue
+      if binary['os'] != 'mac': continue
+      if 'installer' not in binary: continue
+
+      cask_name = 'adoptopenjdk' + str(ver) \
+        + ('' if jvm_impl == 'hotspot' else '-openj9') \
+        + ('' if binary['image_type'] == 'jdk' else '-jre') \
+        + ('' if binary['heap_size'] == 'normal' else '-large')
+      version = rel['version']['openjdk_version']
+      sha256 = binary['installer']['checksum']
+      tuna_url = 'https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/' \
+        + str(ver) + '/' + binary['image_type'] + '/x64/mac/' \
+        + binary['installer']['name']
+      name = 'AdoptOpenJDK' + str(ver) \
+        + ('' if jvm_impl == 'hotspot' else ' (OpenJ9)') \
+        + ('' if binary['image_type'] == 'jdk' else ' (JRE)') \
+        + ('' if binary['heap_size'] == 'normal' else ' (XL)')
+      filename = binary['installer']['name']
+      identifier = 'net.adoptopenjdk.' + str(ver) \
+        + ('' if jvm_impl == 'hotspot' else '-openj9') \
+        + ('' if binary['image_type'] == 'jdk' else '.jre')
+      
+      cask_tmp = cask.replace('{cask_name}', 'tuna-' + cask_name)\
+        .replace('{version}', version)\
+        .replace('{sha256}', sha256)\
+        .replace('{url}', tuna_url)\
+        .replace('{name}', name)\
+        .replace('{filename}', filename)\
+        .replace('{identifier}', identifier)
+      
+      cask_filename = 'Casks/tuna-' + cask_name + '.rb'
+      with open(cask_filename, "w") as f:
+        f.write(cask_tmp)
