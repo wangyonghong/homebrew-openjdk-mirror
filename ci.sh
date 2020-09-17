ed463df532d3829fda4f0819c24ada659b187087
wangyonghong <yonghongwang@163.com>
2020-09-17 19:05:48 +0800

    update

--- ci.sh
+++ ci.sh
@@ -9,6 +9,7 @@ git checkout update
 python3 update_casks.py
 sed -i "s/^Last Update Time.*/Last Update Time: $(date '+%Y-%m-%d %H:%M:%S')/g" README.md
 
+git merge master
 git add .
 git commit -m "update"
 git push -u origin update
