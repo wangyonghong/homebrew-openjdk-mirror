ec0f4e360cc70b337aeddaf2b27040e1af59a43d
wangyonghong <yonghongwang@163.com>
2020-10-02 10:46:26 +0800

    :tada: update

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ git config --global user.email 'yonghongwang@163.com'
 git config --global user.name 'wangyonghong'
 
 git checkout update
-git merge master
+git merge master --allow-unrelated-histories
 
 python3 update_casks.py
 sed -i "s/^Last Update Time.*/Last Update Time: $(date '+%Y-%m-%d %H:%M:%S')/g" README.md
